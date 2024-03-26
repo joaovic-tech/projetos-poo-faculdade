d3a84b6951728b0f67f5db10a2928a879d5238bb
joaovic-tech <joaovictorca2004@gmail.com>
2024-03-26 06:35:44 -0300

    aula sobre `class interface`

---
+++ inter/src/empresa/Animal.java
@@ -0,0 +1,6 @@
+package empresa;
+
+public interface Animal {
+    public void emitirSom();
+    public void dormir();
+}
---
+++ inter/src/empresa/Main.java
@@ -0,0 +1,10 @@
+package empresa;
+
+public class Main {
+    public static void main(String[] args) {
+        Animal gato = new Gato();
+
+        gato.emitirSom();
+        gato.dormir();
+    }
+}
