a6c42d9fa9ed96b616bd1ad071d807ac5031a8b5
joaovic-tech <joaovictorca2004@gmail.com>
2024-03-26 06:57:08 -0300

    aula sobre `enum`

---
+++ enumerador/src/empresa/Main.java
@@ -0,0 +1,8 @@
+package empresa;
+
+public class Main {
+    public static void main(String[] args) {
+        Roupa roupa01 = new Roupa("Camisa", 3, Estacao.VERAO);
+        roupa01.msg();
+    }
+}
